62f3cd478f2c8fc736130c736cfcd03dd447d9c3
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-09-12 21:05:37 -0400

    filter an array

---
+++ 71/scripts.js
@@ -0,0 +1,27 @@
+//filter an array
+
+const scores = [10, 30, 15, 25, 50, 40, 5];
+
+const filteredScrores = scores.filter((score) => {
+    return score > 20;
+});
+
+
+console.log(scores); //filter is non-destructive
+console.log(filteredScrores);
+
+const users = [
+    {name: 'shaun', premium: true},
+    {name: 'yoshi', premium: false},
+    {name: 'mario', premium: false},
+    {name: 'chun-li', premium: true}
+];
+
+const premiumUsers = users.filter(user =>{
+    return user.premium;
+});
+
+//can also write like this:
+//const premiumUsers = users.filter(user => return user.premium);
+
+console.log(premiumUsers);
