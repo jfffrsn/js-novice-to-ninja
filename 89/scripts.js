2218c22b25c2d090cce16f940cb9d16e03ea7de8
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-09-13 22:46:32 -0400

    XMLHttpRequest

---
+++ 89/scripts.js
@@ -0,0 +1,13 @@
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+    //console.log(request, request.readyState);
+
+    //4 is done
+    if(request.readyState === 4){
+        console.log(request.responseText);
+    }
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send();
